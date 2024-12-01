aa46d114c8120eb84d5d1a96f068dabe0d21efbc
aadmkrem <57746412+aadmkrem@users.noreply.github.com>
2024-12-01 11:57:25 +0300

    Add files via upload

---
+++ ualert.py
@@ -0,0 +1,47 @@
+import subprocess
+import time
+import logging
+import json
+from telegram import Bot
+
+# Настройка логирования
+logging.basicConfig(
+    filename='/var/log/device_monitor.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+# Загрузка конфигурации из файла
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+TOKEN = config['TOKEN']
+CHAT_ID = config['CHAT_ID']
+device_hostname = config['device_hostname']
+bot = Bot(token=TOKEN)
+
+def is_device_connected(hostname):
+    try:
+        output = subprocess.check_output(['ping', '-c', '1', hostname])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+last_status = False
+logging.info('Сервис запущен и начинает проверку устройства.')
+while True:
+    try:
+        current_status = is_device_connected(device_hostname)
+
+        if current_status and not last_status:
+            bot.send_message(chat_id=CHAT_ID, text='Смартфон подключен к Wi-Fi!')
+            logging.info('Устройство подключено к Wi-Fi.')
+        elif not current_status and last_status:
+            bot.send_message(chat_id=CHAT_ID, text='Смартфон отключен от Wi-Fi!')
+            logging.info('Устройство отключено от Wi-Fi.')
+
+        last_status = current_status
+        time.sleep(600)  # Проверяем каждые 10 минут
+    except Exception as e:
+        logging.error('Произошла ошибка: %s', e)
+        time.sleep(60)  # Необходимо подождать перед следующей проверкой
